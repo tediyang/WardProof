c2f1116d0f6e0de6a78f165c9c656b657f4af4cd
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-18 22:07:12 +0100

    created the parent model and added it attributes

---
+++ backend/models/parent.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+    This model create the parent data and inherits from
+    the BaseModel.
+
+    Description:
+    1. It provides the following public instaance method:
+      - __setattr__: encrypt the parent password using hashlib.md5
+"""
+
+from models.basemodel import BaseModel, Base
+from sqlalchemy import Column, String, Enum, Date
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class Parent(BaseModel, Base):
+    """ Representation of parent data. """
+    __tablename__ = 'parents'
+    email = Column(String(64), nullable=False, unique=True)
+    password = Column(String(64), nullable=False)
+    first_name = Column(String(64))
+    last_name = Column(String(64))
+    other_name = Column(String(64))
+    gender = Column(Enum('MALE', 'FEMALE', 'OTHERS'))
+    tag = Column(Enum('SUPER-GUARDIAN', 'AUXILLARY-GUARDIAN'))
+    dob = Column(Date)
+    activity = relationship("Activity", uselist=False,
+                            backref="parent", viewonly=True)
+    pupils = relationship("Pupil", backref="parent", cascade="delete")
+    auxillary_guardians = relationship("AuxillaryGuardian",
+                                       backref="parent",
+                                       cascade="delete")
+
+    def __init__(self):
+        """initializes parent"""
+        super().__init__()
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
