75c1ea927b6683d9214e170ee4473862e5bbae81
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-18 22:01:36 +0100

    increase the number of characters in id column

--- backend/models/basemodel.py
+++ backend/models/basemodel.py
@@ -32,7 +32,7 @@ class BaseModel:
     """ All other models will inherit from this BaseModel. """
 
     # Class attributes which other models will inherit.
-    id = Column(String(60), primary_key=True)
+    id = Column(String(64), primary_key=True)
 
     # Generate default values in the database.
     created_at = Column(DateTime, default=datetime.utcnow)
