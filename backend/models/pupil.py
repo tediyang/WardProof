59cb76e88d35d0a79f902aceb88c123d35a37e8f
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-20 16:52:05 +0100

    added allergy column and made viewonly=False

--- backend/models/pupil.py
+++ backend/models/pupil.py
@@ -20,18 +20,19 @@ from sqlalchemy.orm import relationship
 class Pupil(BaseModel, Base):
     """Representation of pupil data"""
     __tablename__ = "pupils"
+    school_id = Column(String(64), ForeignKey('schools.id'), nullable=False)
+    parent_id = Column(String(64), ForeignKey('parents.id')) #Super-Guardian
     first_name = Column(String(64), nullable=False)
     last_name = Column(String(64), nullable=False)
     other_name = Column(String(64))
     dob = Column(Date, nullable=False)
+    allergy = Column(String(128))
     class_grade = Column(String(64), nullable=False)
     gender = Column(Enum('MALE', 'FEMALE', 'OTHERS'), nullable=False)
-    school_id = Column(String(64), ForeignKey('schools.id'), nullable=False)
-    parent_id = Column(String(64), ForeignKey('parents.id')) #Super-Guardian
     parent_fullname = Column(String(64)) #Super-Guardian fullname
 
     activity = relationship("Activity", uselist=False, backref="pupil",
-                            viewonly=True, cascade="all, delete")
+                            cascade="all, delete")
     auxillary_guardians = relationship("AuxillaryGuardian",
                                        backref="pupil",
                                        cascade="delete",
