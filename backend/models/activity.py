132d3021ea5c5bfacbe108140956a473cf2fda9a
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-22 02:29:52 +0100

    activity inherit basemodel public instance attributes

--- backend/models/activity.py
+++ backend/models/activity.py
@@ -29,8 +29,6 @@ class Activity(BaseModel, Base):
     """
 
     __tablename__ = "activities"
-    id = Column(String(64), primary_key=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
     school_id = Column(String(64), ForeignKey("schools.id"), nullable=False)
     pupil_id = Column(String(64), ForeignKey("pupils.id"), nullable=False)
     parent_id = Column(String(64), ForeignKey("parents.id"), nullable=False)
@@ -39,13 +37,5 @@ class Activity(BaseModel, Base):
     action = Column(Enum('PICK-UP', 'DROP-OFF'), nullable=False)
 
     def __init__(self):
-        """
-            Initialize activity
-
-            Description:
-                The public instance attributes of the BaseModel Class were not
-            inherited because the activity should only be created and cannot be
-            updated.
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
+        """initializes parent"""
+        super().__init__()
