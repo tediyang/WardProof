d4bbfddd384afbb911d1c8e8efd580f8aa7b8574
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-22 15:20:15 +0100

    simplified the kwargs public instance attributes

--- backend/models/basemodel.py
+++ backend/models/basemodel.py
@@ -42,18 +42,13 @@ class BaseModel:
         """Initialization of the public instances."""
         if kwargs:
             for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, time)
+                    setattr(self, key, value)
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if "id" not in self.__dict__:
-                self.id = str(uuid.uuid4())
+                if "id" not in self.__dict__:
+                    self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
