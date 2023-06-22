6c2cc162c1c62e9bfd8a80dc9e6509ce1c4d2e52
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-22 02:31:09 +0100

    added keyword arguments in public instance attributes to update class data

--- backend/models/basemodel.py
+++ backend/models/basemodel.py
@@ -38,11 +38,27 @@ class BaseModel:
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
 
-    def __init__(self):
-        """ initialization of the public instance """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialization of the public instances."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if "id" not in self.__dict__:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
 
     def __str__(self):
         """ string representation of the Object class """
