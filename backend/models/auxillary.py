4eb4f11008a20b92edce04eb2b90f5bb7110d4d0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-24 01:09:18 +0100

    created auxillary guardian table

---
+++ backend/models/auxillary.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    This model create the auxillary data and inherits from
+    the BaseModel.
+"""
+
+from models.basemodel import BaseModel, Base
+from sqlalchemy import Column, String, Enum, ForeignKey
+
+
+class AuxillaryGuardian(BaseModel, Base):
+    """
+        Representation of auxillary_guardian data.
+
+        Args:
+            BaseModel (_type_): class
+            Base (_type_): sqlalchemy declarative_base
+    """
+
+    __tablename__ = 'auxillaries'
+    parent_id = Column(String(64), ForeignKey('parents.id'), nullable=False)
+    first_name = Column(String(64), nullable=False)
+    last_name = Column(String(64), nullable=False)
+    other_name = Column(String(64))
+    gender = Column(Enum('MALE', 'FEMALE', 'OTHERS'))
+    tag = Column(Enum('AUXILLARY-GUARDIAN'))
+
+    def __init__(self):
+        """initializes parent"""
+        super().__init__()
