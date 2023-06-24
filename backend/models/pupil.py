991cf1335d7e4cb56d32faa66a9be73a1533b3ef
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-06-24 14:07:07 +0100

    solved comment error and remove blackspace

--- backend/models/pupil.py
+++ backend/models/pupil.py
@@ -3,7 +3,7 @@
 """
     This model create the child data and inherits from
     the BaseModel.
-    
+
     Description:
     1. It provides the following class atrribute relationships:
       - activity: Fetch all the activities carried out with a
@@ -27,7 +27,7 @@ class Pupil(BaseModel, Base):
     """
     __tablename__ = "pupils"
     school_id = Column(String(64), ForeignKey('schools.id'))
-    parent_id = Column(String(64), ForeignKey('parents.id')) #Super-Guardian
+    parent_id = Column(String(64), ForeignKey('parents.id'))  # Super-Guardian
     first_name = Column(String(64), nullable=False)
     last_name = Column(String(64), nullable=False)
     other_name = Column(String(64))
@@ -35,11 +35,10 @@ class Pupil(BaseModel, Base):
     allergy = Column(String(128))
     class_grade = Column(String(64), nullable=False)
     gender = Column(Enum('MALE', 'FEMALE', 'OTHERS'), nullable=False)
-    parent_fullname = Column(String(64)) #Super-Guardian fullname
+    parent_fullname = Column(String(64))  # Super-Guardian fullname
 
     activity = relationship("Activity", uselist=False, backref="pupil",
                             cascade="all, delete")
-
 
     def __init__(self):
         """ Initializes pupil """
