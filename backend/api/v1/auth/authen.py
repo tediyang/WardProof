9f14293d72c73d7e1c08bfc19f6e90d121a6d63a
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-11 23:07:51 +0100

    properly return correct http message and swagged endpoint from api docs file:yml

--- backend/api/v1/auth/authen.py
+++ backend/api/v1/auth/authen.py
@@ -2,7 +2,7 @@
 """
     Authentication for parent and school
 """
-from flask import jsonify, request
+from flask import abort, jsonify, request
 from ..auth import auth_parent
 from ..auth import auth_school
 from models.school import School
@@ -10,12 +10,14 @@ from models.parent import Parent
 from models import storage
 from flask_bcrypt import check_password_hash
 from flask_jwt_extended import create_access_token
+from flasgger.utils import swag_from
 import datetime
 
 
 # Signup
 @auth_school.route('/signup', methods=['POST'], strict_slashes=False)
-def signup():
+@swag_from('docs/authen/school_signup.yml', methods=['POST'])
+def school_signup():
     """
     signup - function that handles signup for school.
 
@@ -23,11 +25,11 @@ def signup():
         json: school id
     """
     body = request.get_json()
-    school = storage.first(School, {'email': body.get('email')})
+    school = storage.validate(School, {'email': body.get('email')})
 
     # if school is true return email already used.
     if school:
-        return jsonify({'error': 'Email address already exists'})
+        abort(400, description='Email address already exists')
 
     new_school = School(**body)
     id = new_school.id
@@ -35,11 +37,12 @@ def signup():
     storage.save()
     storage.close()
 
-    return jsonify({'id': id}), 200
+    return jsonify({'id': id}), 201
 
 
 @auth_parent.route('/signup', methods=['POST'], strict_slashes=False)
-def signup():
+@swag_from('docs/authen/parent_signup.yml', methods=['POST'])
+def parent_signup():
     """
     signup - function that handles signup for parent.
 
@@ -47,11 +50,11 @@ def signup():
         json: parent id
     """
     body = request.get_json()
-    parent = storage.first(Parent, {'email': body.get('email')})
+    parent = storage.validate(Parent, {'email': body.get('email')})
 
     # if parent is true return email already used.
     if parent:
-        return jsonify({'error': 'Email address already exists'}), 401
+        abort(400, description='Email address already exists')
 
     new_parent = Parent(**body)
     id = new_parent.id
@@ -59,12 +62,13 @@ def signup():
     storage.save()
     storage.close()
 
-    return jsonify({'id': id}), 200
+    return jsonify({'id': id}), 201
 
 
 # Login
 @auth_school.route('/login', methods=['POST'], strict_slashes=False)
-def login():
+@swag_from('docs/authen/school_login.yml', methods=['POST'])
+def school_login():
     """
     login - function that handles login session for school.
 
@@ -72,13 +76,13 @@ def login():
         json: access token
     """
     body = request.get_json()
-    school = storage.first(School, {'email': body.get('email')})
+    school = storage.validate(School, {'email': body.get('email')})
     if not school:
-        return jsonify({'error': 'No account registered to this email'}), 401
+        abort(400, description='No account registered to this email')
 
     authorized = check_password_hash(school.password, body.get('password'))
     if not authorized:
-        return jsonify({'error': 'Email or password invalid'}), 401
+        abort(401, description='Email or password invalid')
 
     expires = datetime.timedelta(days=3)
     access_token = create_access_token(identity=school.id,
@@ -88,7 +92,8 @@ def login():
 
 
 @auth_parent.route('/login', methods=['POST'], strict_slashes=False)
-def login():
+@swag_from('docs/authen/parent_login.yml', methods=['POST'])
+def parent_login():
     """
     login - function that handles login session for parent.
 
@@ -96,13 +101,13 @@ def login():
         json: access token
     """
     body = request.get_json()
-    parent = storage.first(Parent, {'email': body.get('email')})
+    parent = storage.validate(Parent, {'email': body.get('email')})
     if not parent:
-        return jsonify({'error': 'No account registered to this email'}), 401
+        abort(400, description='No account registered to this email')
 
     authorized = check_password_hash(parent.password, body.get('password'))
     if not authorized:
-        return jsonify({'error': 'Email or password invalid'}), 401
+        abort(401, description='Email or password invalid')
 
     expires = datetime.timedelta(days=3)
     access_token = create_access_token(identity=parent.id,
